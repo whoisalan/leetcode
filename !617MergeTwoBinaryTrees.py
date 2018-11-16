a60fa0dfb807fd6eeb541320a2ce78da75708965
Alan <alanznala@163.com>
2018-11-16 22:01:52 +0800

    2018年11月16日

---
+++ !617MergeTwoBinaryTrees.py
@@ -0,0 +1,16 @@
+# fail!!!
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 and t2:
+            t1.val += t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            return t1
+        else:
+            return t1 or t2
