70a373d686a8a1a159a4ea98c236f73b953f8543
Alan <alanznala@163.com>
2018-11-27 20:42:30 +0800

    2018年11月27日

--- medium/890FindandReplacePattern.py
+++ medium/890FindandReplacePattern.py
@@ -1,3 +1,5 @@
+
+
 class Solution:
     def findAndReplacePattern(self, words, pattern):
         """
@@ -5,4 +7,11 @@ class Solution:
         :type pattern: str
         :rtype: List[str]
         """
-        
+        b = pattern
+        def is_iso(a):
+            return len(a) == len(b) and len(set(a)) == len(set(b)) == len(set(zip(a, b)))
+        return list(filter(is_iso, words))
+
+# filter(func,list),返回一个迭代器
+# 仅仅返回func判断为真的值
+# zip这个绝了
