0b4c9b7d948e7d8308ad80e237d3745bfe4ba8f0
Alan <alanznala@163.com>
2018-12-26 12:58:25 +0800

    2018年12月26日

--- sort/binarySearch.py
+++ sort/binarySearch.py
@@ -1,5 +1,5 @@
 def binSearch(list1,key1):
-    i,j = 0,len(list1)-1
+    i,j =  0,len(list1)-1
     
     while i<=j :
         mid = (i+j)//2
@@ -7,6 +7,7 @@ def binSearch(list1,key1):
             j = mid-1
         elif key1>list1[mid]:
             i = mid+1
-        else:return mid
-    
+        else:
+            return mid
+
     return -1
---
+++ test.py
@@ -0,0 +1,51 @@
+def maxsum(list1):
+    index1,index2 = 0,0
+    sum = 0
+    for i in range(len(list1)-1):
+        if list1[i] > 0
+            index2+=1
+            sum+=i 
+        if list1[i] < 0:
+            if list1[]
+
+def maxSum(array):
+    length = len(array)
+    sum=array[0]
+    result=array[0]
+
+    for i in range(0,n):
+        if sum<0:
+            sum=a[i]
+        else:
+            sum=sum+a[i]
+            index11=i
+        if sum>result:
+            result=sum
+            index2=i
+            start=start1
+
+    return result,index1,index2
+
+def maxSum(array):
+    length=len(array)
+    sum=array[0]
+    result=array[0]
+
+    for i in range(0,length):
+        if sum<0:
+            sum=a[i]
+        else:
+            sum=sum+a[i]
+            start1=i
+
+        if sum>result:
+            result=sum
+            end=i
+            start=start1
+            
+    return result,start,end
+
+
+
+
+    
